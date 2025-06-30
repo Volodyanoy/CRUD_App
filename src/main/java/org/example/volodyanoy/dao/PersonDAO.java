6253667793a6c12c45dea4c8b80e2975de8ebeb6
Volodyanoy <volodyanoy@gmail.com>
2025-06-30 22:23:44 +0700

    Added form validation

--- src/main/java/org/example/volodyanoy/dao/PersonDAO.java
+++ src/main/java/org/example/volodyanoy/dao/PersonDAO.java
@@ -12,10 +12,10 @@ public class PersonDAO {
 
     {
         people = new ArrayList<>();
-        people.add(new Person(++PEOPLE_COUNT, "Tom"));
-        people.add(new Person(++PEOPLE_COUNT, "Bob"));
-        people.add(new Person(++PEOPLE_COUNT, "Mike"));
-        people.add(new Person(++PEOPLE_COUNT, "Kate"));
+        people.add(new Person(++PEOPLE_COUNT, "Tom", 24, "tom@mail.ru"));
+        people.add(new Person(++PEOPLE_COUNT, "Bob", 52, "bob@mail.ru"));
+        people.add(new Person(++PEOPLE_COUNT, "Mike", 18, "mike@yahoo.com"));
+        people.add(new Person(++PEOPLE_COUNT, "Kate", 34, "kate@gmail.com"));
     }
 
     public List<Person> index(){
@@ -34,6 +34,9 @@ public class PersonDAO {
     public void update(int id, Person updatedPerson){
         Person personToBeUpdated = show(id);
         personToBeUpdated.setName(updatedPerson.getName());
+        personToBeUpdated.setAge(updatedPerson.getAge());
+        personToBeUpdated.setEmail(updatedPerson.getEmail());
+
     }
 
     public void delete(int id){
