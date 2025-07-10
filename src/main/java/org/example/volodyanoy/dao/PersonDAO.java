2ccb0add2f58ed0f95af2ee3cc34c890a3cff15a
Volodyanoy <volodyanoy@gmail.com>
2025-07-10 23:32:37 +0700

    Add autoincrement for field 'id'

--- src/main/java/org/example/volodyanoy/dao/PersonDAO.java
+++ src/main/java/org/example/volodyanoy/dao/PersonDAO.java
@@ -33,7 +33,7 @@ public class PersonDAO {
     }
 
     public void save(Person person){
-        jdbcTemplate.update("INSERT INTO Person VALUES(1, ?, ?, ?)", person.getName(),
+        jdbcTemplate.update("INSERT INTO Person(name, age, email) VALUES(?, ?, ?)", person.getName(),
                 person.getAge(), person.getEmail());
 
     }
@@ -52,7 +52,7 @@ public class PersonDAO {
     }
 
     /// ///////////////////////////////
-    /// Butch Update
+    /// Batch Update
     /// ///////////////////////////////
 
     public void testMultipleUpdate() {
